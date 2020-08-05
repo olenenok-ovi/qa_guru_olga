25597e0e206b011d9a4a64d4b3dcb530d3a1ecc5
Olga Pankova <opankova@fil-it.ru>
2020-08-05 17:25:46 +0300

    lesson1

--- src/test/java/web/GoogleTest.java
+++ src/test/java/web/GoogleTest.java
@@ -1,4 +1,4 @@
-package tests;
+package web;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +8,26 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class GoogleTest {
+
     @Test
-    void selenideSearchTest() {
+    void selenideSearchGoogleTest() {
         // Открыть google
         open("https://google.com");
 
         // Ввести Selenide в поиск
         $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+
+    @Test
+    void selenideSearchYandexTest() {
+        // Открыть yandex
+        open("https://yandex.ru/");
+
+        // Ввести Selenide в поиск
+        $(byName("text")).setValue("Selenide").pressEnter();
 
         // Проверить, что Selenide появился в результатах поиска
         $("html").shouldHave(text("ru.selenide.org"));
--- src/test/java/web/fgh.java
+++ src/test/java/web/fgh.java
@@ -1,4 +1,4 @@
-package tests;
+package web;
 
 public class fgh {
 }
